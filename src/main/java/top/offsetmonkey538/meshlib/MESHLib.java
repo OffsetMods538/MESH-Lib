882d589b1d3128ea5cfe98d80325470dec56d547
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2024-10-12 17:12:53 +0300

    Initial

--- src/main/java/top/offsetmonkey538/meshlib/MESHLib.java
+++ src/main/java/top/offsetmonkey538/meshlib/MESHLib.java
@@ -1,19 +1,23 @@
 package top.offsetmonkey538.meshlib;
 
-import net.fabricmc.api.ModInitializer;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MESHLib implements ModInitializer {
+/**
+ * Just used to store some constants
+ */
+public final class MESHLib {
+	/**
+	 * Private constructor as this class shouldn't be instanced
+	 */
+	private MESHLib() {}
+
+	/**
+	 * String modid for this mod
+	 */
 	public static final String MOD_ID = "mesh-lib";
+	/**
+	 * Logger instance used by this mod
+	 */
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	@Override
-	public void onInitialize() {
-	}
-
-	public static Identifier id(String path) {
-		return new Identifier(MOD_ID, path);
-	}
 }
