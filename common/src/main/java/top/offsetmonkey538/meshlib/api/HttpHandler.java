a009a850a0fae979230b2d8c6fb1637754616dac
OffsetMonkey538 <71213040+OffsetMonkey538@users.noreply.github.com>
2025-10-20 18:10:43 +0200

    fix javadoc compilation

--- common/src/main/java/top/offsetmonkey538/meshlib/api/HttpHandler.java
+++ common/src/main/java/top/offsetmonkey538/meshlib/api/HttpHandler.java
@@ -1,33 +1,16 @@
 package top.offsetmonkey538.meshlib.api;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.DefaultFullHttpResponse;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.FullHttpResponse;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import io.netty.util.CharsetUtil;
 import top.offsetmonkey538.meshlib.api.router.HttpRouterRegistry;
-import top.offsetmonkey538.meshlib.api.router.rule.HttpRule;
 import top.offsetmonkey538.meshlib.example.SimpleHttpHandler;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Function;
 
-import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
-import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
-import static top.offsetmonkey538.meshlib.MESHLib.LOGGER;
-
 /**
  * An http handler for you to implement :D
- * <p>
- * This class also provides some utility methods for making your life easier.
  * <br>
- * For example {@link #sendError(ChannelHandlerContext, HttpResponseStatus)} {@link #sendError(ChannelHandlerContext, HttpResponseStatus, String)}
- * <p>
  * Look at {@link SimpleHttpHandler SimpleHttpHandler} for an example
  *
  * @see HttpRouterRegistry
